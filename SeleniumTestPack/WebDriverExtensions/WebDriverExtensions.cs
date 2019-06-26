4b12cdc21624516951aafb863f2b4701afbbbd91
Dean Akinwale <dean.akinwale@hotmail.com>
2019-06-26 22:30:10 +0100

    added web driver extensions class

---
+++ SeleniumTestPack/WebDriverExtensions/WebDriverExtensions.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestPack.WebDriverExtensions
+{
+    public static class WebDriverExtensions
+    {
+        public static IWebElement WaitForElementToExist(this IWebDriver driver, By element, int timeToWait)
+        {
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeToWait));
+                return wait.Until(d => d.FindElement(element));
+            }
+            catch (NoSuchElementException)
+            {
+                return null;
+            }
+           
+        } 
+    }
+}
